593d3a26c42c45ecf8cd12ccf1b19d8a09f93095
Mats Lindh <mats@lindh.no>
2019-11-14 22:17:34 +0100

    Project setup

---
+++ intellipush/client.py
@@ -0,0 +1,8 @@
+
+class Intellipush:
+    def __init__(self, key, secret, base_url=''):
+        pass
+
+    def sms(self, message, country_prefix, number):
+        pass
+
---
+++ setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(
+    name='intellipush',
+    version='0.1.0',
+    description='Client for accessing intellipush services.',
+    package_dir={'intellipush': 'intellipush'},
+)
