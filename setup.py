ed923ddfbfd9ba3097e76016a607309718a8205a
Mats Lindh <mats@lindh.no>
2019-11-15 21:55:27 +0100

    Add dependencies to setup.py

--- setup.py
+++ setup.py
@@ -1,8 +1,19 @@
 from setuptools import setup
 
+install_requires = [
+    'requests',
+]
+
+tests_require = [
+    'pytest',
+    'pytest-mock',
+]
+
 setup(
     name='intellipush',
     version='0.1.0',
     description='Client for accessing intellipush services.',
     package_dir={'intellipush': 'intellipush'},
+    install_requires=install_requires,
+    tests_require=tests_require,
 )
